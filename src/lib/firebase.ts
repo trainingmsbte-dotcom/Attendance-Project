68cf54004fd5ff89ca9942a7fa1e7d060c8e0c00
training MSBTE <trainingmsbte@gmail.com>
2025-09-27 11:24:46 +0000

    apiKey: "AIzaSyA0XjLn8Ugzjn3pKVkF4rpl248UMQd9ctY",   authDomain: "testve

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -13,7 +13,7 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase for SSR
+// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 
