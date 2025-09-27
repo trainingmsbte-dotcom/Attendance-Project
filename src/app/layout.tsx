5c8a586983013962759b781b76e77448bd2c2230
training MSBTE <trainingmsbte@gmail.com>
2025-09-27 09:59:16 +0000

    page background color is white

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body className={cn("min-h-screen bg-background font-sans antialiased", inter.variable)}>
         {children}
         <Toaster />
