8bd2967733b63e2b10b284c9074d42923dd5ef0f
training MSBTE <trainingmsbte@gmail.com>
2025-09-28 12:24:10 +0000

    then make  pwa

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,11 @@
 import type {NextConfig} from 'next';
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -32,4 +39,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
